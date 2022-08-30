f0d79b2c34df24fb29c220eb8eb54bbdfeedb55a
polomas <benavidesge@outlook.com>
2022-08-29 20:25:20 -0500

    dtfyguhijk

--- utils/data.js
+++ utils/data.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 const data = {
   users: [
     {
-      name: "OSWA",
+      name: "Jhon",
       email: "admin@example.com",
       password: bcrypt.hashSync("123456"),
       isAdmin: true,
